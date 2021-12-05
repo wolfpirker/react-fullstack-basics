af997c63d505c63a04e0e2cd4a0fc7d83f4b2cb2
wolfpirker <w_pirker@gmx.de>
2021-12-05 22:46:15 +0100

    first types of components, and some styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = () => {
+class Header extends Component {
 
-    const getTheYear = () => {
-        const newDate = new Date();
-        return newDate.getFullYear();
+    render() {
+        return (
+            <header>
+                <div className="logo">Logo</div>
+                <input />
+            </header>
+        )
     }
-
-    return(
-        <div>
-            The dat is {5+5}
-        </div>
-    )
 }
 
 export default Header;
