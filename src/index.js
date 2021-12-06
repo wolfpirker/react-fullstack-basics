d1cde7dd7373c3b4975a2447d3cdd41652d5bce3
wolfpirker <w_pirker@gmx.de>
2021-12-06 19:52:55 +0100

    footer component, props part 3

--- src/index.js
+++ src/index.js
@@ -6,22 +6,27 @@ import JSON from './db.json';
 
 import Header from './components/header';
 import NewsList from './components/news_list';
+import Footer from './components/footer';
 
 
 class App extends Component {
 
     state = {
-        news: JSON
+        news: JSON,
+        footerText: 'I am a happy footer'
     }
 
 
     render() {
+        const { news, footerText } = this.state;
+
         return (
             <>
                 <Header />
                 <NewsList
                     news={this.state.news}
                 />
+                <Footer footerText={footerText} />
             </>
         )
     }
