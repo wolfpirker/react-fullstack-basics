0317048af3c4d01ddf70fccb85e4d079659278b2
wolfpirker <w_pirker@gmx.de>
2021-12-06 15:51:23 +0100

    implement props 1

--- src/components/header.js
+++ src/components/header.js
@@ -1,44 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-
-    state = {
-        name: 'Francis',
-        title: 'The keywords are:',
-        keywords: '',
-        count: 0
-    }
-
-    inputChangeHandler = (event) => {
-        this.setState({
-            keywords: event.target.value
-        })
-    }
-
-    addOne() {
-        this.setState((state, props) => ({
-            count: state.count + 1
-        }))
-    }
-
-    render() {
-        // console.log(this.state)
-
-        return (
-            <header>
-                <div className="logo">Logo</div>
-                <input
-                    onChange={this.inputChangeHandler}
-                />
-                <div>{this.state.title}</div>
-                <div>{this.state.keywords}</div>
-
-                <br />
-                <div>{this.state.count}</div>
-                <button onClick={() => this.addOne()}>Add one</button>
-            </header>
-        )
-    }
+const Header = (props) => {
+    return (
+        <header>
+            <div className="logo">Logo</div>
+            <input
+                onChange={props.keywords}
+            />
+        </header>
+    )
 }
 
 export default Header;
